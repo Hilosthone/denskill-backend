43b0565a2f7e52921c21a6dcce59e56d834e85c1
hilosthone <solihullahsulyman@gmail.com>
2026-07-29 13:48:47 +0100

    Add SSL configuration for Render PostgreSQL connection

--- src/controllers/enrollmentController.js
+++ src/controllers/enrollmentController.js
@@ -656,17 +656,29 @@ exports.verifyEnrollmentPayment = async (req, res) => {
         )
       }
 
+      //     return res.redirect(
+      //       `https://denskill.com/student/dashboard?payment=success&reference=${reference}`,
+      //     )
+      //   } else {
+      //     return res.redirect(
+      //       `https://denskill.com/student/dashboard?payment=failed&reference=${reference}`,
+      //     )
+      //   }
+      // } catch (err) {
+      //   console.error('Verification Error:', err.response?.data || err.message)
+      //   return res.redirect('https://denskill.com/student/dashboard?payment=error')
+      // }
       return res.redirect(
-        `https://denskill.com/student/dashboard?payment=success&reference=${reference}`,
+        `http://localhost:3000/student/dashboard?payment=success&reference=${reference}`,
       )
     } else {
       return res.redirect(
-        `https://denskill.com/student/dashboard?payment=failed&reference=${reference}`,
+        `http://localhost:3000/student/dashboard?payment=failed&reference=${reference}`,
       )
     }
   } catch (err) {
     console.error('Verification Error:', err.response?.data || err.message)
-    return res.redirect('https://denskill.com/student/dashboard?payment=error')
+    return res.redirect('http://localhost:3000/student/dashboard?payment=error')
   }
 }
 
