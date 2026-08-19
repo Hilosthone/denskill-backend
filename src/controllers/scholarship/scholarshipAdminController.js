fac0706d199f15987a239dfae4a83383d35f6b98
hilosthone <solihullahsulyman@gmail.com>
2026-08-19 15:45:15 +0100

    feat: update, delete and activate ready cohorts

--- src/controllers/scholarship/scholarshipAdminController.js
+++ src/controllers/scholarship/scholarshipAdminController.js
@@ -311,9 +311,6 @@ const getScholarshipDashboardMetrics = async (req, res) => {
     `
 
     const statsResult = await db.query(statsQuery, params)
-    // const cohortResult = await db.query(
-    //   `SELECT * FROM scholarship_cohorts WHERE status = 'ACTIVE' LIMIT 1`,
-    // )
     const cohortResult = await db.query(
       `SELECT * FROM scholarship_cohorts WHERE UPPER(status) = 'ACTIVE' LIMIT 1`,
     )
